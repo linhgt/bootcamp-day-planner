33378f317d7a12cf4c74f7251ae5281080c4447c
Linh Nguyen <linhnhatmen@gmail.com>
2020-10-06 15:38:21 -0700

    build a time block then append it to the planner

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
+    //array of times for reference
     var Times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    var container = $("#planner");
     
+    //Build a time block
+    function buildBlock(time){
+        //build the outer block
+        var block = $("<div>");
+        block.attr("id", time);
+        block.attr("class", "row border-top border-bottom");
+
+        //Col for displaying time
+        var timeCol = $("<div>");
+        timeCol.addClass("col-md-1");
+        
+        //Entry box
+        var textEntry = $("<textarea>");
+        textEntry.addClass("col-md-10");
+        
+        //save buton
+        var saveBtn = $("<button>");
+        saveBtn.addClass("col-md-1");
+        var saveIcon = $("<i>");
+        saveIcon.addClass("far fa-save");
+        saveBtn.append(saveIcon);
+
+        //Append the time col, entry box and save button to the block
+        block.append(timeCol);
+        block.append(textEntry);
+        block.append(saveBtn);
+
+        //Append the time block to the planner
+        container.append(block);
+    }
 });
